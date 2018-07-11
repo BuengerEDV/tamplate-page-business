580a95872e6beb5f48594856976272107dfc94ca
Nick Bünger <admin@buenger-edv.de>
2018-07-11 22:34:51 +0200

    11072018 - First Backup

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,8 +4,29 @@ $(document).ready(function() {
   $('html').addClass('js'); // Check for javascript
   $('.nav').prepend('<span class="nav-button">☰</span>');
 
-  $( "#menu_btn" ).click(function() {
-    navState = $('#nav').hasClass('nav_open');
+  $( ".nav-button" ).click(function() {
+    var navState = $('.nav-list').hasClass('nav-list-open');
     if(navState == true) {navclose();}else{navopen();}
   });
 });
+
+function navopen() {
+  $('.nav-list').addClass('nav-list-open');
+
+  var intTime = 0;
+  $('.nav-list li').each(function(i){
+    var _this = this;
+    window.setTimeout(function(){$(_this).addClass('nav-list-item-open');}, intTime);
+    intTime += 200;
+  });
+}
+
+function navclose(callback) {
+  var intTime = 0;
+  $('.nav-list li').each(function(i){
+    var _this = this;
+    window.setTimeout(function(){$(_this).removeClass('nav-list-item-open');}, intTime);
+    intTime += 200;
+  });
+  $('.nav-list').removeClass('nav-list-open');
+}
